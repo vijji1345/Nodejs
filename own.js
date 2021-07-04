b0fefe39f7cfe942127c9aa36dfee04e4911b1a5
Nihansh Reddy <Mrvijj1345@outlook.com>
2021-07-04 11:29:53 +0530

    new changes on jsfile

---
+++ own.js
@@ -0,0 +1,12 @@
+function x() {
+    for (var i = 1; i<=10; i++) {
+        
+            setTimeout(function () {
+                console.log(i);
+            }, i*1000);
+        }
+        
+        
+    }
+
+x();
